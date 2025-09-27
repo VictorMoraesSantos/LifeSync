7e3b20892b0476d5f689c860e04c2364d4dc3623
Victor Moraes <victormoraes.2002@hotmail.com>
2025-09-27 16:54:12 -0300

    dada

--- Services/Notification/Notification.Domain/Entities/EmailMessage.cs
+++ Services/Notification/Notification.Domain/Entities/EmailMessage.cs
@@ -5,7 +5,7 @@ namespace EmailSender.Domain.Entities
     public class EmailMessage : BaseEntity<int>
     {
         public string From { get; set; }
-        public string To { get; private set; }
+        public string To { get; private set; }     
         public string Subject { get; private set; }
         public string Body { get; private set; }
 
